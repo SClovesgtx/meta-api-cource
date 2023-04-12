5132d7dc8e78332f798f4cd1be744e7a0392a7f9
Cloves Paiva <cloves.paiva@extremedigital.com.br>
2023-04-12 13:58:42 -0300

    parse result

--- booklist/BookListAPI/views.py
+++ booklist/BookListAPI/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
-# from django.db import TheIntegrityErrorexception
-# from django.http import JsonResponsefunction
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.forms.models import model_to_dict
+
+from .models import Book
 
 # Create your views here.
+@csrf_exempt
+def books(request):
+    if request.method == "GET":
+        calledbooks = Book.objects.all().values()
+        return JsonResponse({"books": list(calledbooks)})
+    elif request.method == "POST":
+        title = request.POST.get("title")
+        author = request.POST.get("author")
+        price = request.POST.get("price")
+        book = Book(title, author, price)
+        try:
+            book.save()
+        except:
+            raise IntegrityError()
+            return JsonResponse({"error": True, "message": "required field missing"}, status=400)
+        return JsonResponse(model_to_dict(book), status=201)
