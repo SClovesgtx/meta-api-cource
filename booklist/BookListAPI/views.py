bc69bc9a13ee96b645c16442775ed21f339aef4d
Cloves Paiva <cloves.paiva@extremedigital.com.br>
2023-04-24 09:43:38 -0300

    DRF

--- booklist/BookListAPI/views.py
+++ booklist/BookListAPI/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from django.db import IntegrityError
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 from .models import Book
 
 # Create your views here.
-@csrf_exempt
+@api_view(["GET", "POST"])
 def books(request):
     if request.method == "GET":
         calledbooks = Book.objects.all().values()
-        return JsonResponse({"books": list(calledbooks)})
+        return Response({"books": list(calledbooks)}, status=status.HTTP_200_OK)
     elif request.method == "POST":
         title = request.POST.get("title")
         author = request.POST.get("author")
@@ -21,5 +23,5 @@ def books(request):
             book.save()
         except:
             raise IntegrityError()
-            return JsonResponse({"error": True, "message": "required field missing"}, status=400)
-        return JsonResponse(model_to_dict(book), status=201)
+            return Response({"error": True, "message": "required field missing"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(model_to_dict(book), status=status.HTTP_201_CREATED)
