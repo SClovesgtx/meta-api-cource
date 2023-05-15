b173f9c98c2825856d277e67b3e5ab56be2e62c0
Cloves Paiva <cloves.paiva@extremedigital.com.br>
2023-05-15 09:03:24 -0300

    class baed view

--- booklist/BookListAPI/views.py
+++ booklist/BookListAPI/views.py
@@ -1,27 +1,31 @@
 from django.shortcuts import render
-from django.db import IntegrityError
-from django.http import JsonResponse
-from django.forms.models import model_to_dict
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
-from .models import Book
 
-# Create your views here.
 @api_view(["GET", "POST"])
 def books(request):
-    if request.method == "GET":
-        calledbooks = Book.objects.all().values()
-        return Response({"books": list(calledbooks)}, status=status.HTTP_200_OK)
-    elif request.method == "POST":
-        title = request.POST.get("title")
-        author = request.POST.get("author")
-        price = request.POST.get("price")
-        book = Book(title, author, price)
-        try:
-            book.save()
-        except:
-            raise IntegrityError()
-            return Response({"error": True, "message": "required field missing"}, status=status.HTTP_400_BAD_REQUEST)
-        return Response(model_to_dict(book), status=status.HTTP_201_CREATED)
+    return Response("List of books", status=status.HTTP_200_OK)
+
+
+class BookList(APIView):
+    def get(self, request):
+        author = request.GET.get("author")
+        if author:
+            return Response(
+                {"message": f"List of books of author {author}."},
+                status=status.HTTP_200_OK,
+            )
+        return Response({"message": "List of books"}, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        return Response({"message": "new book created"}, status=status.HTTP_201_CREATED)
+
+
+class Book(APIView):
+    def get(self, request, pk):
+        return Response(
+            {"message": f"Single book with id {pk}."}, status=status.HTTP_200_OK
+        )
