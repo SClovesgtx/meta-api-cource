a061af77cac7ab611dc5cdc0ce29ed4ab44b12b7
Cloves Paiva <cloves.paiva@extremedigital.com.br>
2023-05-18 08:12:09 -0300

    LittleLemon

---
+++ LittleLemon/LittleLemonAPI/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemsView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
--- booklist/BookListAPI/urls.py
+++ booklist/BookListAPI/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     # path('books/', views.books, name='books')
     path("books", views.BookList.as_view()),
-    path("book/<int:pk>", views.Book.as_view()),
+    path("books/<int:pk>", views.Book.as_view()),
 ]
