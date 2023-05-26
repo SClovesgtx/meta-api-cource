aa35050cb76617aaa7ca61bbaec092e209f4f8d5
Cloves Paiva <cloves.paiva@extremedigital.com.br>
2023-05-26 09:17:24 -0300

    filtering and ordering

--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("menu-items", views.MenuItemsView.as_view()),
+    path("menu-items", views.menu_items),
     path("menu-item/<int:pk>", views.SingleMenuItemsView.as_view()),
     path("menu", views.menu),
     path("", views.welcome),
--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -4,13 +4,38 @@ from .serializers import MenuItemSerializer
 from rest_framework.renderers import TemplateHTMLRenderer, StaticHTMLRenderer
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.response import Response
-from rest_framework_csv.renderers import CSVRenderer
+from rest_framework import status
+
+# from rest_framework_csv.renderers import CSVRenderer
 
 
-@renderer_classes([CSVRenderer])
-class MenuItemsView(generics.ListCreateAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+# class MenuItemsView(generics.ListCreateAPIView):
+#     queryset = MenuItem.objects.all()
+#     serializer_class = MenuItemSerializer
+
+
+@api_view(["GET", "POST"])
+def menu_items(request):
+    if request.method == "GET":
+        items = MenuItem.objects.all()
+        to_price = request.query_params.get("to_price")
+        title = request.query_params.get("title")
+        ordering = request.query_params.get("ordering")
+        if to_price:
+            items = items.filter(price__gte=to_price)
+        if title:
+            items = items.filter(title__icontains=title)
+        if ordering:
+            ordering_fields = ordering.split(",")
+            items = items.order_by(*ordering_fields)
+        serialized_item = MenuItemSerializer(items, many=True)
+        return Response(serialized_item.data)
+    elif request.method == "POST":
+        serializer = MenuItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_201_CREATED)
+        return Response(serializer.errors)
 
 
 class SingleMenuItemsView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
